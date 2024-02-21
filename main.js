2ec8f17c03c44517c3e7567e67dd38ea729e2e40
Kunal Upadhyay <81215146+Kunal-Upadhyay@users.noreply.github.com>
2024-02-21 16:12:48 +0530

    First Commit
    First Commit Defining the first look of website with a working frontend
     - by Kunal Upadhyay from S.W.A.G. Dev

---
+++ main.js
@@ -0,0 +1,15 @@
+let tnc=document.getElementById("info");
+let mnf=document.getElementById("form");
+let nav=document.getElementById("nav");
+function nxt(){
+    tnc.style.display="none";
+    mnf.style.display="block";
+}
+function shnav() {
+    if (nav.style.display=="inline") {
+        nav.style.display="none";
+    } else {
+        nav.style.display="inline";
+    }
+    
+}
